2b1e28eb45cef11fc6982354388382f0898b9ea3
Viktor Novikov <novikovamg3@icloud.com>
2019-08-13 15:01:40 +0300

    Initial Commit

---
+++ WorldEyeMobile/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WorldEyeMobile
+//
+//  Created by Viktor Novikov on 13.08.2019.
+//  Copyright © 2019 Viktor Novikov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ WorldEyeMobileTests/WorldEyeMobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  WorldEyeMobileTests.swift
+//  WorldEyeMobileTests
+//
+//  Created by Viktor Novikov on 13.08.2019.
+//  Copyright © 2019 Viktor Novikov. All rights reserved.
+//
+
+import XCTest
+@testable import WorldEyeMobile
+
+class WorldEyeMobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
